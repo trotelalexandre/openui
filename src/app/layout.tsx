9d56c70f804dc8f964d2b3f96481a7e39139bd91
Alexandre Trotel <trotelalexandre@proton.me>
2025-02-22 21:40:51 +0100

    fix: remove spinner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NextTopLoader />
+          <NextTopLoader showSpinner={false} />
           <Navbar isAuthenticated={isAuthenticated} />
 
           {children}
